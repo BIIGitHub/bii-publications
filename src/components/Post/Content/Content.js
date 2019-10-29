fa779cd8b524725109bdced7f66706b4ed28b219
sumhm@bii.a-star.edu.sg <sumhm@bii.a-star.edu.sg>
2019-10-29 17:48:58 +0800

    Adding fields to template:

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -4,13 +4,23 @@ import styles from './Content.module.scss';
 
 type Props = {
   body: string,
-  title: string
+  title: string,
+  journaltypes: string,
+  journal: string,
+  pubmed: string,
+  url: string,
+  impactfactor: string
 };
 
-const Content = ({ body, title }: Props) => (
+const Content = ({ body, title, journaltypes, journal, pubmed, url, impactfactor }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
-    <span>Abstract: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+    <span>Journal Type: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: journaltypes }} />
+    <span>Journal: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: journal }} />
+    <span>Pubmed: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: pubmed }} />
+    <span>URL: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: url }} />
+    <span>Impact Factor: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: impactfactor }} />
+    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
   </div>
 );
 
