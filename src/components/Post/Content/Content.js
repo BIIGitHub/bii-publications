bd2e4bc206d0bc51dc84edf72313f7ddccb296fa
sumhm@bii.a-star.edu.sg <sumhm@bii.a-star.edu.sg>
2019-10-29 17:25:47 +0800

    update files

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -10,7 +10,6 @@ type Props = {
 const Content = ({ body, title }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
-    <span>Journal Types: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: journaltypes }} />
     <span>Journal: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: journal }} />
     <span>Pubmed: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: pubmed }} />
     <span>URL: </span><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: url }} />
