a98af21e2395bb4bb576f7b16b20799bf6a9b5d5
sumhm@bii.a-star.edu.sg <sumhm@bii.a-star.edu.sg>
2019-10-30 10:14:45 +0800

    trying to make it work

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   subtitle: 'Repository of all published Science Articles by our Researchers, their Group Members and Collaborators.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
-  postsPerPage: 4,
+  postsPerPage: 5,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
