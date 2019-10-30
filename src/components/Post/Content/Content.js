576b05a7a38e72bfc64daf88f5f58242f06d937a
sumhm@bii.a-star.edu.sg <sumhm@bii.a-star.edu.sg>
2019-10-30 17:16:34 +0800

    adding break lines

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -6,11 +6,11 @@ import styles from './Content.module.scss';
 const Content = ({ body, title }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
-    <b>Journal Type:</b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journaltypes }} />
-    <b>Journal: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journal }} />
-    <b>Pubmed: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.pubmed }} />
-    <b>URL:</b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.url }} />
-    <b>Impact Factor: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.impactfactor }} />
+    <b>Journal Type:</b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journaltypes }} /> <br />
+    <b>Journal: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journal }} /> <br />
+    <b>Pubmed: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.pubmed }} /> <br />
+    <b>URL:</b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.url }} /> <br />
+    <b>Impact Factor: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.impactfactor }} /> <br /> 
     <div className={styles['content__body']}>
       <p>{body.description}</p>
     </div>
