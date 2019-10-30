6f32d9dfbe686f3750846d4fd528cc9c960535e8
sumhm@bii.a-star.edu.sg <sumhm@bii.a-star.edu.sg>
2019-10-30 16:58:08 +0800

    representation of journal details

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -6,11 +6,11 @@ import styles from './Content.module.scss';
 const Content = ({ body, title }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
-    <b><span>Journal Type: </span></b><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journaltypes }} />
-    <b><span>Journal: </span></b><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journal }} />
-    <b><span>Pubmed: </span></b><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.pubmed }} />
-    <b><span>URL: </span></b><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.url }} />
-    <b><span>Impact Factor: </span></b><div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.impactfactor }} />
+    <b>Journal Type:</b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journaltypes }} />
+    <b>Journal: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journal }} />
+    <b>Pubmed: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.pubmed }} />
+    <b>URL:</b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.url }} />
+    <b>Impact Factor: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.impactfactor }} />
     <div className={styles['content__body']}>
       <p>{body.description}</p>
     </div>
