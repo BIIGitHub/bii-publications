9edde73d9ba18ddfb73b5c99d4fc2c7a63849d25
sumhm@bii.a-star.edu.sg <sumhm@bii.a-star.edu.sg>
2019-10-31 17:05:55 +0800

    revert preview

--- src/cms/preview-templates/post-preview.js
+++ src/cms/preview-templates/post-preview.js
@@ -9,32 +9,12 @@ type Props = {
 
 const PostPreview = ({ entry, widgetFor }: Props) => {
   const body = widgetFor('body');
-  const journaltypes = widgetFor('journaltypes');
-  const journal = widgetFor('journal');
-  const pubmed = widgetFor('pubmed');
-  const url = widgetFor('url');
-  const impactfactor = widgetFor('impactfactor');
-  const dateofacceptance = widgetFor('dateofacceptance');
-  const author = widgetFor('tag');
-  const categories = widgetFor('categories');
   const title = entry.getIn(['data', 'title']);
 
   return (
     <div className="post">
       <h1 className="post__title">{title}</h1>
-      <div className="post__body">
-          Journal Types: {journaltypes} <br />
-          Journal: {journal} <br />
-          Pubmed: {pubmed} <br />
-          URL: {url} <br />
-          Impact Factor: {impactfactor} <br />
-          Date of Acceptance: {dateofacceptance} 
-      </div>
       <div className="post__body">{body}</div>
-      <div className="post__body">
-          Author: {author} <br />
-          Group: {categories} <br />
-      </div>
     </div>
   );
 };
