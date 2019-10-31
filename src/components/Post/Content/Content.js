ce5a5456de945187c9ae71a6e0508c11c4841372
Seaw Ker Boon <seawkerboon@gmail.com>
2019-10-31 17:30:05 +0800

    Conditional Rendering of Pubmed

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -8,7 +8,12 @@ const Content = ({ body, title }: Props) => (
     <h1 className={styles['content__title']}>{title}</h1>
     <b>Journal Type:</b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journaltypes }} /> <br />
     <b>Journal: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.journal }} /> <br />
-    <b>Pubmed: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.pubmed }} /> <br />
+    {body.pubmed &&
+      <div>
+        <b>Pubmed: </b>&nbsp;
+        <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.pubmed }} /> <br />
+      </div>    
+    }
     <b>URL:</b>&nbsp; <a href={body.url}><span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.url }} /></a> <br />
     <b>Impact Factor: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.impactfactor }} /> <br /> 
     <b>Date of Acceptance: </b>&nbsp; <span className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body.dateofacceptance }} /> <br /> 
